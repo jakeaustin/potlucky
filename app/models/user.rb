ac4639c69495c8073546ec8924fbed0379b4931e
Jake Austin <jakeaust07@gmail.com>
2014-07-10 11:51:25 -0400

    delete unnecessary source: options

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,11 +9,11 @@ class User < ActiveRecord::Base
   has_many :hosted_meals, through: :hosted_invites, dependent: :destroy, source: :meal
 
   has_many :guest_invites, -> { where role: 'guest', invite_status: 'attending' }, class_name: Invite
-  has_many :guest_meals, through: :guest_invites, source: :user, source: :meal
+  has_many :guest_meals, through: :guest_invites, source: :meal
   has_many :invited_invites, -> { where role: 'guest', invite_status: 'pending' }, class_name: Invite
-  has_many :invited_meals, through: :invited_invites, source: :user, source: :meal
+  has_many :invited_meals, through: :invited_invites, source: :meal
   has_many :declined_invites, -> { where role: 'guest', invite_status: 'decline' }, class_name: Invite
-  has_many :declined_meals, through: :declined_invites, source: :user, source: :meal
+  has_many :declined_meals, through: :declined_invites, source: :meal
 
 
 
