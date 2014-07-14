005032ca650d994bb9328f30ac259b1763ed0acc
Jake Austin <jakeaust07@gmail.com>
2014-07-13 22:12:58 -0400

    style dish#search, add flash fading

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,9 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
+
+$(function() {
+   $('.notice').delay(300).fadeIn('normal', function() {
+      $(this).delay(2500).fadeOut();
+   });
+});
